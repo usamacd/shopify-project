7822643228752b4c5180f31fdc77563f8b003a78
Usama_Saud <usama.saud@codedistrict.com>
2022-06-22 16:14:31 +0500

    new

---
+++ assets/slider.js
@@ -0,0 +1,31 @@
+
+  $(".slider-slick").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+
+  });
+
+  $('.video-slider-content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true
+  });
+
+  $('.testimonialsCarousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots:true
+  });
+
+  $('.slick-carousel').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+
+  });
+      
+
